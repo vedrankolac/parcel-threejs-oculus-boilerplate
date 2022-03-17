e474a31a44a2e180fa324dedc1726114bccf1e98
birdlaketree <birdlaketree@gmail.com>
2022-03-17 21:04:15 +0100

    set camera and fog far properties so the objects disapear in far distance

--- src/js/World/components/camera.js
+++ src/js/World/components/camera.js
@@ -1,7 +1,7 @@
 import { PerspectiveCamera } from 'three';
 
 const createCamera = () => {
-  const camera = new PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 0.1, 100 );
+  const camera = new PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 0.1, 200 );
   camera.position.z = 20;
   return camera;
 }
--- src/js/World/components/scene.js
+++ src/js/World/components/scene.js
@@ -4,9 +4,9 @@ import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment
 
 const createScene = renderer => {
   const scene = new Scene();
-  scene.background = new Color( 0xcccccc );
+  scene.background = new Color( 0xffffff );
   
-  const fog = new Fog( 0x222222, 0, 60 );
+  const fog = new Fog( 0xffffff, 0, 200 );
   scene.fog = fog;
   
   const pmremGenerator = new PMREMGenerator(renderer);
